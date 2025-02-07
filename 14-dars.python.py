f240b847ce61d6f11c33d7cf7e863d75e26ed332
Dildoraxon <obidovadildora54@gmail.com>
2025-02-07 11:50:19 +0500

    Yangi dars qo'shildi

---
+++ 14-dars.python.py
@@ -0,0 +1,23 @@
+'''
+23/12/2024
+Dasturlash asoslari
+#14-dars: DICTIONARY (LUG'AT)
+Muallif: Dildoraxon Obidova
+'''
+
+
+#car_0 = {"model":"ferrari","rang":"qizil"}
+#print(car_0["model"])
+#print(car_0["rang"])
+
+
+#en_uz = {"apple":"olma", "apricot": "oʻrik", "banana": "banan"}
+#print(en_uz["apple"])
+
+
+#mevalar = {"olma": 8000, "banan": 15000, "ananas": 25000}
+#print(f"Olma narhi {mevalar["olma"]} soʻm")
+
+
+talaba_0 = {"ism": "dildora obidova","yosh": 18, "t_yil": 2006}
+print(f"{talaba_0["ism"].title()}, {talaba_0["t_yil"]}-yilda tugʻilgan, {talaba_0["yosh"]} yoshda")
